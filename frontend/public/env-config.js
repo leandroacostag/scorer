730ac4888019c1ec2b31fcd728ad6019b9075be4
Leandro Nicolás Acosta <leandro.acosta@getrightdata.com>
2025-03-14 20:32:13 -0300

    Update project configuration and dependencies
    - Added `env-config.js` to `.gitignore` to prevent sensitive information from being tracked.
    - Updated `pnpm-lock.yaml` to reflect changes in dependencies, including updates to `type-fest` and the addition of new packages like `serve` and `clipboardy`.
    - Removed the `env-config.js` file from the frontend public directory to enhance security by eliminating hardcoded environment variables.

--- frontend/public/env-config.js
+++
@@ -1,5 +0,0 @@
-window.env = {
-    AUTH0_DOMAIN: 'dev-5y88eq51hrwk8iop.us.auth0.com',
-    AUTH0_CLIENT_ID: 'DfeC7Gu4CUDwYQK3AmuNDuuL9gkHhlrM',
-    AUTH0_AUDIENCE: 'https://dev-5y88eq51hrwk8iop.us.auth0.com/api/v2/'
-};
